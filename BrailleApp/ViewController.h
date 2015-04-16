facbe20c751a3dd24afae79730c7bf08809c3e7e
Ismael Salas Martos <ismaelsm@MacBook-Pro-de-Ismael.local>
2015-04-16 17:21:48 +0200

    Initial Commit

---
+++ BrailleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrailleApp
+//
+//  Created by Ismael Salas Martos on 16/4/15.
+//  Copyright (c) 2015 Ismael Salas Martos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BrailleApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BrailleApp
+//
+//  Created by Ismael Salas Martos on 16/4/15.
+//  Copyright (c) 2015 Ismael Salas Martos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
